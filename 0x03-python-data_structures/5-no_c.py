d516425183ae273d51942040536ca89d1304abc6
nohaelnagar <eng.noha17@gmail.com>
2023-10-09 19:41:30 +0200

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for letter in my_string:
-        if letter != 'c' or letter != 'C':
-            new_string += letter
-    return (new_string)
+    new_string = [letter for letter in my_string if letter != 'c' or letter != 'C']
+    return ("".join(new_string))
