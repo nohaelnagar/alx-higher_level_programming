9bb4492142160a3d4321f281f088236a6079a377
nohaelnagar <eng.noha17@gmail.com>
2023-10-09 19:26:39 +0200

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,6 @@
-#!/usr/bin/python3
-
+def no_c(my_string):
+    new_string = ''
+    for letter in my_string:
+        if letter != 'c' and letter != 'C':
+            new_string += letter
+    return new_string
