87fa1301442f4431b598c0f31f9ce4f0239791bc
nohaelnagar <eng.noha17@gmail.com>
2023-10-09 20:38:54 +0200

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,15 +6,3 @@ def print_matrix_integer(matrix=[[]]):
             if y != (len(matrix[x]) - 1):
                 print(" ", end="")
         print("")
-
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
