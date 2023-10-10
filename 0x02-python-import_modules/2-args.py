557a0af13345981ecb53379191ce43e46404462b
nohaelnagar <eng.noha17@gmail.com>
2023-10-11 01:47:09 +0200

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 import sys
 if __name__ == "__main__":
 
-    num_args = (len(sys.argv) -1)
-    if num_args == 0:
-        print("{} arguments.".format(num_args))
-    elif num_args == 1:
-        print("{} argument:".format(num_args))
-    else:
-        print("{} arguments:".format(num_args))
-    for num in range(1, len(sys.argv)):
-        print("{}: {}".format(num, sys.argv[num]))
+	num_args = (len(sys.argv) -1)
+	if num_args == 0:
+		print("{} arguments.".format(num_args))
+	elif num_args == 1:
+		print("{} argument:".format(num_args))
+	else:
+		print("{} arguments:".format(num_args))
+	for num in range(1, len(sys.argv)):
+		print("{}: {}".format(num, sys.argv[num]))
