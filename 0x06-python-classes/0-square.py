70bd3a5dfd4c373bfb46ee9ff088cc6f18c61fba
nohaelnagar <eng.noha17@gmail.com>
2023-10-24 17:49:22 +0200

    task1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Empty Square class
-"""
+""" Empty Square class """
 
 
 class Square:
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Empty Square class """
+
+
+class Square:
+    """ class Square that defines a squar """
+
+    def __init__(self, size):
+        """
+            size
+        """   
+        
+        self.__size = size
