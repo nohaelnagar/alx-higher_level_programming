0b6543e788c50f3c92c59776c4ec81aad2389a19
nohaelnagar <eng.noha17@gmail.com>
2023-10-24 19:40:19 +0200

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,11 +15,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -28,6 +28,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def area(self):
         return self.__size ** 2
