d053c7eb698e28bd47a7f28abb8f21bce7979f4c
nohaelnagar <eng.noha17@gmail.com>
2023-10-29 22:40:06 +0100

    0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""0-add_integer
+"""
+
+
+def add_integer(a, b=98):
+    """add_integer
+
+    Args:
+        a (int or float): first value to add
+        b (int or float): second value to add, Defaults to 98.
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
