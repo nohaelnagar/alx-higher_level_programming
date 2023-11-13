591049597bceeebac91864a2d559ac4376275752
nohaelnagar <eng.noha17@gmail.com>
2023-11-13 23:47:35 +0100

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,12 +6,10 @@ class Base:
     """ New class base """
 
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects +=1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
-    
