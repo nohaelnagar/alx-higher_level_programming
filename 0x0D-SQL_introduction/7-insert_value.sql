f0b2aff5c55605138eac99e283911dbf788dd3fe
nohaelnagar <eng.noha17@gmail.com>
2024-03-15 03:41:18 +0100

    7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- Inserts a new row in the table first_table
-INSERT INTO first_table IF EXISTS IN hbtn_0c_0
-VALUE (89, "Best School");
+INSERT INTO first_table(id, name) VALUE (89, "Best School");
