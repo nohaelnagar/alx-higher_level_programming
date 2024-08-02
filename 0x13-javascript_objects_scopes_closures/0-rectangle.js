6871888d9028103bccf698b62ad83be22779c574
nohaelnagar <eng.noha17@gmail.com>
2024-08-02 06:52:59 +0200

    javascript 2

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-// code made by : youssef
 class Rectangle {}
 module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// code made by : youssef
 class Rectangle {
     constructor (w, h) {
       if (w > 0 && h > 0) {
